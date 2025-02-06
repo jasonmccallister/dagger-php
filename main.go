d17379bfb675223884f849d082868455eedbab58
Jason McCallister <jason@dagger.io>
2025-02-06 10:31:47 -0500

    add support for xdebug, run to stdout
    Signed-off-by: Jason McCallister <jason@dagger.io>

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"dagger/dagger-setup-php/internal/dagger"
 	"fmt"
 )
 
 type Php struct {
+	EnableXdebug bool
+
 	// The PHP extensions to install
 	Extensions []string
 
@@ -22,10 +25,15 @@ func New(
 	// +optional
 	// +default=["bcmath","cli","common","curl","intl","mbstring","mysql","opcache","readline","xml","zip"]
 	extensions []string,
+	// Enable Xdebug for the PHP container
+	// +optional
+	// +default=false
+	enableXdebug bool,
 ) *Php {
 	return &Php{
-		Version:    version,
-		Extensions: extensions,
+		EnableXdebug: enableXdebug,
+		Version:      version,
+		Extensions:   extensions,
 	}
 }
 
@@ -36,7 +44,11 @@ func (m *Php) Setup() *dagger.Container {
 		ext = append(ext, fmt.Sprintf("php%s-%s", m.Version, e))
 	}
 
-	return dag.Container().
+	if m.EnableXdebug {
+		ext = append(ext, fmt.Sprintf("php%s-xdebug", m.Version))
+	}
+
+	c := dag.Container().
 		From("ubuntu:24.04").
 		WithEnvVariable("DEBIAN_FRONTEND", "noninteractive").
 		WithExec([]string{"apt", "update", "-y"}).
@@ -46,12 +58,19 @@ func (m *Php) Setup() *dagger.Container {
 		WithExec(append([]string{"apt", "install", "-y", "php" + m.Version}, ext...)).
 		WithWorkdir("/app").
 		WithEntrypoint([]string{"php"})
+
+	if m.EnableXdebug {
+		c = c.WithEnvVariable("XDEBUG_MODE", "coverage")
+	}
+
+	return c
 }
 
 // Run a PHP command with the specified arguments
 func (m *Php) Run(
+	ctx context.Context,
 	// The arguments to pass to the PHP command
 	args []string,
-) *dagger.Container {
-	return m.Setup().WithExec(append([]string{"php"}, args...))
+) (string, error) {
+	return m.Setup().WithExec(append([]string{"php"}, args...)).Stdout(ctx)
 }
